97fc9c2b92c7acfadaa1468b5b61d7aabd486aab
Michal Olszewski <molszews@thoughtworks.com>
2013-12-05 11:52:45 +0100

    java cookbook is now configured to download latest java 7

---
+++ my-cookbooks/application/attributes/default.rb
@@ -0,0 +1,15 @@
+override[:java] = {
+    :install_flavor => 'oracle',
+    :jdk_version => '7',
+    :jdk => {
+        "7" => {
+            :x86_64 => {
+                :url => 'http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz',
+                :checksum => 'f2eae4d81c69dfa79d02466d1cb34db2b628815731ffc36e9b98f96f46f94b1a'
+            }
+        }
+    },
+    :oracle => {
+        :accept_oracle_download_terms => true
+    }
+}
